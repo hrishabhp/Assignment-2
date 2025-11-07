1fe2c9d303d849fbde2fb6a2e609ec5d321e47ce
Hrishabh Chandra Paal <hrishabh.pal@gmail.com>
2025-11-07 15:51:42 +0530

    Add files via upload

---
+++ Sum_of_interger_using_loop.py
@@ -0,0 +1,9 @@
+# Initialize sum to 0
+total_sum = 0
+
+# Iterate over numbers from 1 to 50
+for number in range(1, 51):
+    total_sum += number
+
+# Display the final sum
+print(f"The sum of all integers from 1 to 50 is: {total_sum}")
